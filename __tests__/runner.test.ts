648f569f667a621c7f7a0c9353900a7975738590
Artur Melanchyk <artur.melanchyk@veertu.com>
2022-10-31 18:26:30 +0100

    remove runner test

--- __tests__/runner.test.ts
+++
@@ -1,9 +0,0 @@
-import {Octokit} from '@octokit/rest'
-import {Runner} from '../src/runner'
-import {expect, test} from '@jest/globals'
-
-test('starts VM successfully', async () => {
-  const octokit = jest.mock('@octokit/rest')
-  const runner = new Runner(octokit, 'owner', 'repo')
-})
-
